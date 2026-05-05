a8fbe540cc943bd88ec9e5653684404aa6d09eab
Paolo Lopez <okihnnj123@gmail.com>
2026-05-05 09:12:06 -0500

    Fix - Navbar Tailwind

--- restaurante-nextjs/app/components/NavBar.tsx
+++ restaurante-nextjs/app/components/NavBar.tsx
@@ -6,22 +6,20 @@ import { usePathname } from 'next/navigation';
 
 export default function NavBar() {
     const pathname = usePathname();
-
     // Función helper — detecta si la ruta está activa
-    const esActiva = (ruta: string): string =>
-        pathname === ruta
-            ? 'font-bold text-blue-700 underline'
-            : 'text-gray-700 hover:text-blue-600';
+    const esActiva = (ruta: string): string => pathname === ruta
+        ? 'font-bold text-red-700'
+        : 'text-gray-700 hover:text-white-600';
 
     return (
         <nav className='bg-white shadow px-6 py-3 flex gap-6'>
             <span className='font-bold text-lg mr-4'>🍽 Restaurante</span>
             <ul className='flex gap-6'>
-                <ol><Link href='/' className={esActiva('/')}>Home</Link></ol>
-                <ol><Link href='/mesas' className={esActiva('/mesas')}>Mesas</Link></ol>
-                <ol><Link href='/menu' className={esActiva('/menu')}>Menú</Link></ol>
-                <ol><Link href='/carrito' className={esActiva('/carrito')}>Carrito</Link></ol>
+                <li><Link href='/' className={esActiva('/')}>Home</Link></li>
+                <li><Link href='/mesas' className={esActiva('/mesas')}>Mesas</Link></li>
+                <li><Link href='/menu' className={esActiva('/menu')}>Menú</Link></li>
+                <li><Link href='/carrito' className={esActiva('/carrito')}>Carrito</Link></li>
             </ul>
-        </nav>
+        </nav >
     );
 }
